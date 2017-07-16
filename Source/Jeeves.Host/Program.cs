af7e0996e37ba98eda4f18f4d70c4af257734f64
Florian Winkelbauer <mail@florianwinkelbauer.com>
2017-07-16 13:14:42 +0200

    Move data migration into host proejct

--- Source/Jeeves.Host/Program.cs
+++ Source/Jeeves.Host/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
+using DbUp;
 using Jeeves.Core;
 using Jeeves.Host.Properties;
 
@@ -9,18 +11,64 @@ namespace Jeeves.Host
     {
         public static void Main()
         {
+            var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            var database = new FileInfo(Path.Combine(userFolder, "Jeeves.sqlite"));
+
+            MigrateDatabase(database);
+            StartServer(database);
+        }
+
+        private static void MigrateDatabase(FileInfo database)
+        {
+            WriteColorLine("Migrating database", ConsoleColor.Magenta);
+
+            var connectionString = $"Data Source = {database}";
+
+            var upgrader = DeployChanges.To
+                .SQLiteDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (result.Successful)
+            {
+                WriteColorLine("Finished migration!", ConsoleColor.Green);
+            }
+            else
+            {
+                WriteColorLine(result.Error.Message, ConsoleColor.Red);
+                Console.WriteLine("Press ENTER to exit");
+                Console.ReadLine();
+
+                Environment.Exit(2);
+            }
+        }
+
+        private static void StartServer(FileInfo database)
+        {
+            var url = Settings.Default.BaseUrl;
+            var store = new SQLiteStore(database);
             var settings = new JeevesSettings(Settings.Default.UseHttps, Settings.Default.UseAuthentication);
 
-            var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            var file = new FileInfo(Path.Combine(userFolder, "Jeeves.sqlite"));
-            var store = new SQLiteStore(file);
+            WriteColorLine($"Starting Jeeves on {url}", ConsoleColor.Magenta);
 
-            using (var host = new JeevesHost(settings, store, new Uri(Settings.Default.BaseUrl)))
+            using (var host = new JeevesHost(settings, store, new Uri(url)))
             {
                 host.Start();
                 Console.WriteLine("Press ENTER to exit");
                 Console.ReadLine();
             }
+        }
+
+        private static void WriteColorLine(string line, ConsoleColor color)
+        {
+            var tmp = Console.ForegroundColor;
+
+            Console.ForegroundColor = color;
+            Console.WriteLine(line);
+            Console.ForegroundColor = tmp;
         }
     }
 }
